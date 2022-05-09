0876f8a6d346f0232692956e3c3eab32dcea7734
mdavidson05 <maxtondavidson@gmail.com>
2022-05-09 13:50:13 +0100

    First Commit

---
+++ Static_Testing.py
@@ -0,0 +1,23 @@
+class CardGame:
+
+
+  def check_for_ace(self, card):
+    if card.value = 1: #should be ==, = is to assign a variable but this is checking if values are equals
+      return True
+    else #missing colon
+      return False
+   
+
+  dif highest_card(self, card1 card2): #dif instead of def when defining the function
+  if card1.value > card2.value:
+    return card #should be return card 1
+  else:
+    return card2
+  
+
+
+def cards_total(self, cards):
+  total # should be total = 0 to start a running total. Currently cards will be added to nothing
+  for card in cards:
+    total += card.value
+    return "You have a total of" + total
